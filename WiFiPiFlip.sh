e40007c0fa5e335f1941bbceeb90a40dd3e43cd6
bradtaco <bradtaco@users.noreply.github.com>
2024-02-06 15:24:58 -0800

    Update WiFiPiFlip.sh
    Moved the requests for network info to the network reconnect function.

--- WiFiPiFlip.sh
+++ WiFiPiFlip.sh
@@ -21,7 +21,6 @@ fi
 
 # Function to check and display the WiFi mode
 check_wifi_mode() {
-    # Use iw to get the current mode of the WiFi interface
     local mode=$(iw dev $WIFI_INTERFACE info | grep 'type' | awk '{print $2}')
     if [ -z "$mode" ]; then
         echo "Unable to determine the current mode of $WIFI_INTERFACE."
@@ -43,6 +42,12 @@ enable_monitor_mode() {
 
 # Function to switch back to managed mode and attempt to reconnect to WiFi
 reconnect_wifi() {
+    # Request SSID and WiFi Password when needed
+    echo "Detected wireless interface: $WIFI_INTERFACE"
+    read -p "Enter SSID for reconnection: " WIFI_SSID
+    read -s -p "Enter WiFi Password: " WIFI_PASSWORD
+    echo
+
     echo "Switching $WIFI_INTERFACE back to managed mode..."
     sudo ip link set $WIFI_INTERFACE down
     sudo iw $WIFI_INTERFACE set type managed
@@ -64,27 +69,14 @@ show_menu() {
 
     case $choice in
         1) enable_monitor_mode ;;
-        2) reconnect_wifi ;;
+        2) 
+            # Initial setup to guess WiFi interface
+            WIFI_INTERFACE=$(iw dev | awk '$1=="Interface"{print $2}' | head -n 1)
+            reconnect_wifi ;;
         3) echo "Exiting."; exit 0 ;;
         *) echo "Invalid selection." ;;
     esac
 }
-
-# Initial setup to guess WiFi interface and SSID
-WIFI_INTERFACE=$(iw dev | awk '$1=="Interface"{print $2}' | head -n 1)
-WIFI_SSID=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d':' -f2)
-
-# Ask the user to confirm or input the WiFi details
-echo "Detected wireless interface: $WIFI_INTERFACE"
-read -p "Enter wireless interface [$WIFI_INTERFACE]: " input_interface
-WIFI_INTERFACE=${input_interface:-$WIFI_INTERFACE}
-
-echo "Detected SSID: $WIFI_SSID"
-read -p "Enter SSID [$WIFI_SSID]: " input_ssid
-WIFI_SSID=${input_ssid:-$WIFI_SSID}
-
-read -s -p "Enter WiFi Password: " WIFI_PASSWORD
-echo
 
 # Loop the menu
 while true; do
